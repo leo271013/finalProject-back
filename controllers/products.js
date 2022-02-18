a9953082c08cb924ba547fea11a3895ac42197dd
leo271013@gmail.com <leo271013@gmail.com>
2022-02-18 16:21:29 +0800

    break

--- controllers/products.js
+++ controllers/products.js
@@ -23,3 +23,73 @@ export const getProducts = async (req, res) => {
     res.status(500).send({ success: false, message: '伺服器錯誤' })
   }
 }
+
+export const editProducts = async (req, res) => {
+  const data = {
+    name: req.body.name,
+    class: req.body.class,
+    state: req.body.state,
+    barter: req.body.barter,
+    quantity: req.body.quantity,
+    goal: req.body.goal,
+    userId: req.body.userId,
+    introTitle: req.body.introTitle,
+    introContent: req.body.introContent
+  }
+  if (req.file) {
+    data.image = req.file.path
+  }
+  if (req.body.sale) {
+    data.sale = req.body.sale
+  }
+  try {
+    const result = await products.findByIdAndUpdate(req.params.id, data, { new: true, runValidators: true })
+    res.status(200).send({ success: true, message: '', result })
+  } catch (error) {
+    if (error.name === 'CastError') {
+      res.status(404).send({ success: false, message: '找不到' })
+    } else if (error.name === 'ValidationError') {
+      const key = Object.keys(error.errors)[0]
+      res.status(400).send({ success: false, message: error.errors[key].message })
+    } else {
+      res.status(500).send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+}
+
+export const delProducts = async (req, res) => {
+  try {
+    const result = await products.findByIdAndDelete(req.params.id)
+    if (result) {
+      res.status(200).send({ success: true, message: '' })
+    } else {
+      res.status(404).send({ success: false, message: '查無帳號' })
+    }
+  } catch (error) {
+    if (error.name === 'CastError') {
+      res.status(404).send({ success: false, message: '查無帳號' })
+    } else {
+      // 未知錯誤
+      res.status(500).send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+}
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const result = await products.find({ sale: true })
+    res.status(200).send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+}
+
+export const getClassProducts = async (req, res) => {
+  console.log(req.params.class)
+  try {
+    const result = await products.find({ sale: true, class: req.params.class })
+    res.status(200).send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500).send({ success: false, message: '伺服器錯誤' })
+  }
+}
--- routes/products.js
+++ routes/products.js
@@ -5,12 +5,20 @@ import upload from '../middleware/upload.js'
 
 import {
   postproducts,
-  getProducts
+  getProducts,
+  editProducts,
+  delProducts,
+  getAllProducts,
+  getClassProducts
 } from '../controllers/products.js'
 
 const router = express.Router()
 
-router.post('/', auth, content('multipart/form-data'), upload, postproducts)
+router.get('/:class', getClassProducts)
 router.get('/:id', getProducts)
+router.patch('/:id', auth, content('multipart/form-data'), upload, editProducts)
+router.delete('/:id', auth, delProducts)
+router.post('/', auth, content('multipart/form-data'), upload, postproducts)
+router.get('/', getAllProducts)
 
 export default router
